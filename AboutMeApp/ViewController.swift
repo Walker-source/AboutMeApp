b20bf60bbdf1e9f8f37b3a51228085caa43ac38c
MaDoG <aliasgraphics@icloud.com>
2025-02-08 21:20:02 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Denis Lachikhin on 08.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
