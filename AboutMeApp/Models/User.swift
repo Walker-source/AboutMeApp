e405f29e7afea5587126d0526cbc77df600e75e9
MaDoG <aliasgraphics@icloud.com>
2025-02-11 01:13:50 +0300

    User structure

--- AboutMeApp/Models/User.swift
+++ AboutMeApp/Models/User.swift
@@ -9,10 +9,26 @@ struct User {
     let login: String
     let password: String
     let person: Person
+    
+    static let userCard = User(
+        login: "Denis",
+        password: "Password",
+        person: Person(
+            name: "Денис",
+            surname: "Лачихин",
+            age: 38,
+            company: "InfiniteArtSolutions",
+            department: "3D",
+            pisition: "Freelance"
+        )
+    )
 }
 
 struct Person {
     let name: String
     let surname: String
     let age: Int
+    let company: String
+    let department: String
+    let pisition: String
 }
