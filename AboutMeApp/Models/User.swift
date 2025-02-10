6c060ed5f66f549a0300587a30b935c15066dfed
MaDoG <aliasgraphics@icloud.com>
2025-02-11 00:28:04 +0300

    Commit

---
+++ AboutMeApp/Models/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  AboutMeApp
+//
+//  Created by Denis Lachikhin on 11.02.2025.
+//
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+}
+
+struct Person {
+    let name: String
+    let surname: String
+    let age: Int
+}
