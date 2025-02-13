b2ec20c546e351496b8c2041f7182138897ec1c3
MaDoG <aliasgraphics@icloud.com>
2025-02-13 23:03:16 +0300

    User model fix

--- AboutMeApp/Models/User.swift
+++ AboutMeApp/Models/User.swift
@@ -10,23 +10,10 @@ struct User {
     let password: String
     let person: Person
     
-    static let userCard = User(
+    static let user = User(
         login: "Denis",
         password: "Password",
-        person: Person(
-            name: "Денис",
-            surname: "Лачихин",
-            age: 38,
-            company: "InfiniteArtSolutions",
-            department: "3D",
-            position: "Freelance",
-            biography: """
-   Ничего не понимающий дед. Но очень старающийся. 
-   Родился в Крыму и прожил там 29 лет. Затем переехал в Краснодар, где успешно обосновался. Но затем пришлось снова переехать. В этот раз в Тбилиси. Пожив там чуть больше года очень сильно полюбил Грузию. По определенным обстоятельствам пришлось вернуться. Но очень хочется обратно. 
-   Работаю в геймдеве лет 15 на позиции 3Д художника. Все хорошо, но вот попробовал прогать и очень зашло. Решил стать на эту "кривую" дорожку. 
-""",
-            image: "Denis"
-        )
+        person: .person
     )
 }
 
@@ -34,9 +21,48 @@ struct Person {
     let name: String
     let surname: String
     let age: Int
+    let company: Company
+    
+    var fullName: String {
+        name + " " + surname
+    }
+    
+    static let person = Person(
+        name: "Denis",
+        surname: "Lachikhin",
+        age: 38,
+        company: .company
+    )
+}
+
+struct Company {
     let company: String
     let department: String
     let position: String
+    let biography: Biography
+    
+    static let company = Company(
+        company: "InfiniteArtSolutions",
+        department: "3D",
+        position: "Freelance",
+        biography: .biography
+    )
+}
+
+struct Biography {
     let biography: String
+    let userPhoto: UserImage
+    
+    static let biography = Biography(
+        biography: """
+        Ничего не понимающий дед. Но очень старающийся. Родился в Крыму и прожил там 29 лет. Затем переехал в Краснодар, где успешно обосновался. Но затем пришлось снова переехать. В этот раз в Тбилиси. Пожив там чуть больше года очень сильно полюбил Грузию. По определенным обстоятельствам пришлось вернуться. Но очень хочется обратно. Работаю в геймдеве лет 15 на позиции 3Д художника. Все хорошо, но вот попробовал прогать и очень зашло. Решил стать на эту "кривую" дорожку.
+        """,
+        userPhoto:.userImage
+    )
+}
+
+struct UserImage {
     let image: String
+    
+    static let userImage = UserImage(image: "Denis")
 }
